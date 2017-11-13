48315e4943ff498844007e5a90b0b87c53362a4f
Evert van de Waal <evert.vandewaal@xs4all.nl>
2017-11-13 12:05:51 +0100

    Experimenting with Unix sockets.

---
+++ test/test_server.py
@@ -0,0 +1,121 @@
+
+from unittest import TestCase
+from threading import Thread
+import os, os.path
+import time
+import asyncio
+import socket
+import threading
+
+from admingen.dbengine import readconfig
+from admingen.htmltools import simpleCrudServer, runServer, Page
+from admingen.servers import *
+
+
+TESTDB = 'test.db'
+
+
+
+class ServerTests(TestCase):
+    def setUp(self):
+        # Delete the database if there is one left-over
+        if False and os.path.exists(TESTDB):
+            os.remove(TESTDB)
+
+    def testCrudServer(self):
+        """ Test the simple CRUD server generated on the tables of uren_crm """
+        # Parse the uren_crm details
+        with open('uren_crm.txt') as f:
+            transitions, db, dbmodel = readconfig(f)
+        # Instantiate the database
+        db.bind(provider='sqlite', filename=TESTDB, create_db=True)
+        db.generate_mapping(create_tables=True)
+
+        # Create the server
+        server = simpleCrudServer(dbmodel, Page)
+        # Run the server in a thread
+        runner = Thread(target=runServer, args=(server, {}))
+        runner.setDaemon(True)
+        runner.start()
+
+        while True:
+            time.sleep(1)
+
+    def testKeyring(self):
+
+        @keychain_unlocker('test.enc')
+        class Test:
+            Page = staticmethod(Page)
+
+            @cherrypy.expose
+            def index(self):
+                return self.Page('Hello, World!')
+
+        cherrypy.quickstart(Test(), '/')
+
+
+    def testUnixSockets1(self):
+        path = '/home/ehwaal/tmp/testsock'
+        loop = asyncio.get_event_loop()
+
+        async def handler(reader, writer):
+            while True:
+                data = await reader.readline()
+                print (data)
+                if b'exit' in data:
+                    loop.stop()
+                    return
+                writer.writelines([data])
+
+        server = asyncio.start_unix_server(handler, path)
+        se = loop.create_task(server)
+
+        async def testcase():
+            reader, writer = await asyncio.open_unix_connection(path)
+            writer.write(b'test\n')
+            d = await reader.readline()
+            print ('>>>', d)
+            writer.write(b'Hallo, wereld!\n')
+
+            writer.write(b'exit\n')
+
+        loop.create_task(testcase())
+        loop.run_forever()
+
+    def testUnixSockets(self):
+        path = '/home/ehwaal/tmp/testsock'
+        if os.path.exists(path):
+            os.remove(path)
+
+        loop = asyncio.get_event_loop()
+
+        def testcase():
+            while not os.path.exists(path):
+                time.sleep(0.1)
+
+            s = socket.socket(socket.AF_UNIX)
+            s.connect(path)
+
+            s.send(b'test\n')
+            d = s.recv(1024)
+            print ('>>>', d)
+            s.send(b'Hallo, wereld\n')
+            s.send(b'exit\n')
+
+        async def handler(reader, writer):
+            while True:
+                data = await reader.readline()
+                print (data)
+                if b'exit' in data:
+                    loop.stop()
+                    return
+                writer.writelines([data])
+
+        server = asyncio.start_unix_server(handler, path)
+        se = loop.create_task(server)
+
+        th = threading.Thread(target=testcase)
+        th.setDaemon(True)
+        th.start()
+
+        loop.run_forever()
