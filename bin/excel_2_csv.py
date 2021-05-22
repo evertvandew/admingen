d9e38df99ce7b62e0673927e6986ad99b86a2a49
Evert van de Waal <evert@vdwi-software.nl>
2021-05-22 22:12:39 +0200

    Added an association editor for proces inputs and outputs.

---
+++ bin/excel_2_csv.py
@@ -0,0 +1,4 @@
+#!/usr/bin/env python3
+""" Convert a multi-tab Excel file to a CSV file in my database form.
+"""
+
--- src/admingen/xml_template.py
+++ src/admingen/xml_template.py
@@ -281,9 +281,10 @@ class DataContext:
         if query[0] == '/':
             # First determine which columns to draw
             path = urllib.parse.urlparse(query).path
-            table = path.split('/')[-1]
-            if (path[0] or path[1]) in url_prefixes:
-                source = url_prefixes[path[0] or path[1]]
+            pathparts = path.split('/')
+            table = pathparts[-1]
+            if (pathparts[0] or pathparts[1]) in url_prefixes:
+                source = url_prefixes[pathparts[0] or pathparts[1]]
                 specs = data_models[source][table]
             else:
                 # This is an unknown data source. Only process parameters
@@ -728,7 +729,8 @@ def handle_Template(args, template_lines):
             expand_self = io.StringIO(
                 debug_render(template, lines, render_context))
         except:
-            msg = '<An error occurred when rendering template for %s>\n'%tag
+            msg = '<An error occurred when rendering template for %s>\n'%(tag, args)
+            msg += '\n'.join(lines)
             msg += exceptions.text_error_template().render()
             print(msg, file=sys.stderr)
             raise
